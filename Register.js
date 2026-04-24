7e9a2eee5f7315814e24e14cf75d6cc3e7d0addd
JodiM1206 <dmar9989@gmail.com>
2026-04-24 15:48:00 -0400

    made User.js an export

--- Register.js
+++ Register.js
@@ -1,7 +1,8 @@
+import User from "./User.js";
 let form = document.getElementById("registerForm");
 form.addEventListener('submit', register);
 function register(e){
     e.preventDefault();
     const createUser = new User(document.getElementById("fname").value, document.getElementById("lname").value, document.getElementById("uname").value, document.getElementById("email").value, document.getElementById("passwd").value);
-    console.log(createUser);
+    console.log(createUser.toString());
 }
--- User.js
+++ User.js
@@ -1,9 +1,12 @@
-class User{
+export default class User{
     constructor(firstName, lastName, username, email, password){
         this.firstName = firstName;
         this.lastName = lastName;
         this.username = username;
         this.email = email;
         this.password = password;
+    }
+    toString(){
+        return `User: ${this.firstName} ${this.lastName}, ${this.username}, ${this.email}`;
     }
 }
